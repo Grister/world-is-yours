a64bc3beac307a216d01abc948dc68afee09317d
Grister <carroll.lewi>
2024-04-22 11:59:46 +0300

    provide whitenoise

--- store/store/settings.py
+++ store/store/settings.py
@@ -200,10 +200,10 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 STORAGES = {
